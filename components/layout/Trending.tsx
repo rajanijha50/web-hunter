821b496da681f296005f1d1c37caf2b11148a0db
Rajani Ranjan Jha <rajanijha50@gmail.com>
2026-07-17 12:23:23 +0530

    added new categories

--- components/layout/Trending.tsx
+++ components/layout/Trending.tsx
@@ -11,25 +11,49 @@ export default function Trending() {
   const websites = useWebsiteStore((state) => state.websites);
 
   const [trendingWebsites, setTrendingWebsites] = useState<WebsiteType[]>([]);
+  const [count, setCount] = useState(0);
+  const [targetCount, setTargetCount] = useState(0);
 
   useEffect(() => {
     if (websites.length > 0) {
+      console.log("length: ", websites.length)
       const sorted = [...websites].sort((a, b) => b.likesCount - a.likesCount);
       setTrendingWebsites(sorted.slice(0, 10));
+      setTargetCount(Math.floor(websites.length / 100) * 100);
     }
   }, [websites]);
+
+  useEffect(() => {
+    if(targetCount === 0) return;
+    let startTimestamp: number | null = null;
+    const duration = 2500; // 2 seconds animation 
+    const step = (timestamp: number) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      const easeOutQuart = 1 - Math.pow(1 - progress, 4);
+      setCount(Math.floor(easeOutQuart * targetCount));
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }, [targetCount]);
+
   return (
     <>
       {/* Trending Now Marquee Banner */}
       {trendingWebsites.length > 0 && (
         <section className="animate-fade-in py-12 bg-background overflow-hidden border-t">
-          <div className="mb-8 px-4 md:px-8">
-            <h2 className="text-3xl md:text-4xl font-extrabold tracking-tight text-center">
+          <div className="mb-8 px-4 md:px-8 flex flex-col justify-center items-center gap-4">
+            <h2 className="text-3xl md:text-4xl font-extrabold tracking-tight text-center ">
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-primary to-amber-400">
                 Trending Now
               </span>
             </h2>
-          </div>
+            <span className="mx-auto text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-primary to-amber-400">
+              {count}+ websites and counting
+            </span>
+          </div> 
 
           <div
             className="group relative w-full"
